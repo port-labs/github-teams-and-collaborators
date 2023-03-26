a55ca874769f4b2cefb2eb12ea91d4c5912296e6
danielsinai <daniel.sinai.main@gmail.com>
2023-03-26 16:31:43 +0300

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ githubToken="$INPUT_TOKEN"
 githubRepo="$INPUT_REPO"
 portClientId="$INPUT_PORT_CLIENT_ID"
 portClientSecret="$INPUT_PORT_CLIENT_SECRET"
+blueprintIdentifier="$INPUT_BLUEPRINT_IDENTIFIER"
 
 echo "Getting collaborators and teams for $githubRepo"
 
@@ -15,7 +16,6 @@ collaborators=$(curl \
   "https://api.github.com/repos/${githubRepo}/collaborators" \
   | jq -r '.[].login')
 
-echo "Collaborators: $collaborators"
 
 # Get Repo Teams
 teams=$(curl \
@@ -25,8 +25,53 @@ teams=$(curl \
   "https://api.github.com/repos/${githubRepo}/teams" \
   | jq -r '.[].slug')
 
-echo "Teams: $teams"
 
 # Create output variables
 echo "::set-output name=collaborators::$collaborators"
 echo "::set-output name=teams::$teams"
+
+access_token=$(curl --location --request POST 'https://api.getport.io/v1/auth/access_token' --header 'Content-Type: application/json' --data-raw '{
+	"clientId": "$portClientId",
+	"clientSecret": "$portClientSecret",
+}' | jq '.accessToken' | sed 's/"//g')
+
+echo "Validating if a blueprint with the identifier $blueprintIdentifier identifier exists in Port"
+
+response=$(curl -X 'GET' \
+  'https://api.getport.io/v1/blueprints/$blueprintIdentifier' \ 
+  --header 'Content-Type: application/json' \
+  --header 'Authorization: Bearer $access_token' \
+  --write-out '%{http_code}' \
+  --silent \
+  --output /dev/null)
+
+if [ $response -eq 200 ]; then
+  echo "Blueprint with the identifier $blueprintIdentifier exists in Port"
+else
+  curl -X 'POST' \
+    'https://api.getport.io/v1/blueprints' \
+    --header 'Content-Type: application/json' \
+    --header 'Authorization: Bearer $access_token' \
+    -d '{
+        "identifier": "$blueprintIdentifier",
+        "title": "$blueprintIdentifier",
+        "icon": "Service",
+        "schema": {
+          "properties": {
+            "collaborators": {
+                "type": "array",
+                "title": "Collaborators",
+                "items": {
+                    "type": "string",
+                    "format": "user"
+                }
+            }
+          },
+          "required": [
+          ]
+        },
+        "calculationProperties": {},
+        "mirrorProperties": {},
+        "relations": {}
+      }'
+fi
